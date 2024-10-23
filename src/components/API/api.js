505b6ccc24e981ea10529affaad72ab1d4c6c124
srinidhigopalan <srinidhigo@gmail.com>
2024-10-23 19:26:13 +0530

    first commit

--- src/components/API/api.js
+++ src/components/API/api.js
@@ -1,7 +1,7 @@
 
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:4000'; 
+const API_BASE_URL = 'https://backend-quicksell.onrender.com'; 
 export const fetchTicketsByUser = async () => {
     try {
         const response = await axios.get(`${API_BASE_URL}/get-id`);
